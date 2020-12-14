ffdad7a662bb8618d1f2dcc05fdfac7ad3ef0639
Marcelocoelhop <=>
2020-12-14 13:43:47 -0300

    test: Implementação de upload de todos os arquivos e menu

--- js/scrollPage.js
+++ js/scrollPage.js
@@ -2,9 +2,15 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
     var currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
-        document.querySelector(".main-menu-bottom").style.bottom = "0";
+        document.querySelector(".menu_opc").style.top = "0";
+        document.querySelector("#qtd_manut_pendentes").style.top = "16px";
+        document.querySelector(".main-menu-bars").style.top = "4px";
+    } else if (prevScrollpos < currentScrollPos) {
+        document.querySelector(".menu_opc").style.top = "-80px";
+        document.querySelector("#qtd_manut_pendentes").style.top = "-80px";
+        document.querySelector(".main-menu-bars").style.top = "-80px";
     } else {
-        document.querySelector(".main-menu-bottom").style.bottom = "-80px";
+        console.log('entrou');
     }
     prevScrollpos = currentScrollPos;
 }

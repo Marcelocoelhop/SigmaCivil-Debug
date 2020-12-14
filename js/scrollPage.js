14fc37dad6f79b018b42b3b2d7599bbd7ab69c75
Marcelocoelhop <=>
2020-12-14 14:50:06 -0300

    test: Implementação de upload de todos os arquivos e menu

--- js/scrollPage.js
+++ js/scrollPage.js
@@ -1,16 +1,27 @@
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
     var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.querySelector(".menu_opc").style.top = "0";
-        document.querySelector("#qtd_manut_pendentes").style.top = "16px";
-        document.querySelector(".main-menu-bars").style.top = "4px";
-    } else if (prevScrollpos < currentScrollPos) {
-        document.querySelector(".menu_opc").style.top = "-80px";
-        document.querySelector("#qtd_manut_pendentes").style.top = "-80px";
-        document.querySelector(".main-menu-bars").style.top = "-80px";
-    } else {
-        console.log('entrou');
+    if (tem_conexao()) {
+        if (prevScrollpos > currentScrollPos) {
+            document.querySelector(".menu_opc").style.top = "0";
+            document.querySelector("#qtd_manut_pendentes").style.top = "16px";
+            document.querySelector(".main-menu-bars").style.top = "4px";
+        } else if (prevScrollpos < currentScrollPos) {
+            document.querySelector(".menu_opc").style.top = "-80px";
+            document.querySelector("#qtd_manut_pendentes").style.top = "-80px";
+            document.querySelector(".main-menu-bars").style.top = "-80px";
+        } else {
+            console.log('entrou');
+        }
+
+        prevScrollpos = currentScrollPos;
     }
-    prevScrollpos = currentScrollPos;
+}
+
+function tem_conexao() {
+    if (navigator.connection.type == 0 || navigator.connection.type == 'none') {
+        return false;
+    } else {
+        return true;
+    }
 }
